5eb2c6e91fdbcf4fe9b77bc10d338a0bb7e2ea61
liupeng <1269861316@qq.com>
2024-06-28 16:55:12 +0800

    添加配置文件

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+/*
+ * @Description: 
+ * @Author: sky
+ * @Date: 2024-06-28 16:53:46
+ * @LastEditTime: 2024-06-28 16:53:56
+ * @LastEditors: sky
+ */
+// @ts-check
+
+/** @type {import("prettier").Config} */
+export default {
+    bracketSpacing: true,
+    singleQuote: false,
+    arrowParens: "avoid",
+    trailingComma: "none"
+  };
