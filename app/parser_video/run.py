2c1cd799cb3392f72ede5395840783859de9feb1
liupeng <1269861316@qq.com>
2024-08-02 09:34:50 +0800

    更新run文件

--- app/parser_video/run.py
+++ app/parser_video/run.py
@@ -1,12 +1,5 @@
-'''
-Description: 
-Author: sky
-Date: 2024-08-02 09:15:33
-LastEditTime: 2024-08-02 09:16:15
-LastEditors: sky
-'''
+import subprocess
 import os
 
-
-
-os.system('scrapy crawl sync_player_url')
+basedir = os.path.abspath(os.path.dirname(__file__))
+subprocess.run(['scrapy', 'crawl', 'sync_player_url'], cwd=basedir)
