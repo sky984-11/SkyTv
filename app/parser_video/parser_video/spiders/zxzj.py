3aa64537d0d4d23c4cd16bed0a6bb53bf94e6d68
liupeng <1269861316@qq.com>
2024-08-14 15:32:32 +0800

    fixed zxzj

--- app/parser_video/parser_video/spiders/zxzj.py
+++ app/parser_video/parser_video/spiders/zxzj.py
@@ -26,49 +26,85 @@ class ZxzjSpider(scrapy.Spider):
         return spider
 
     def start_requests(self):
-        #title + source + type确定唯一video,后续缓存以此为key存储url
+        # 1:电影，2,3,4,5剧集，6动漫
+        # 每页12个，不足12则为最后一页
+        # ,2,3,4,5,6
+        type_list = [1]
         if self.base_url:
-            yield Request(
-                    url=self.base_url,
-                    callback=self.parse,
+            for type_num in type_list:
+                start_url = f'/list/{type_num}-1.html'
+                yield Request(
+                    url=''.join([self.base_url,start_url.format(type_num=type_num)]),
+                    callback=self.parse_page,
+                    meta= {
+                        "vod_type" : '电影' if type_num == 1 else '动漫' if type_num == 6 else '剧集'
+                    }
                 )
-
+    def get_next_page_number(self, current_number): 
+	        # 假设页码是整数，并且从2开始（因为已经有一个3-2的页面了）  
+            current_number = int(current_number.split('-')[-1])  
+            return current_number + 1  
+  
+    def get_next_page_url(self, current_url):  
+        # 提取页码部分并递增  
+        page_match = current_url.rsplit('-', 1)[-1].rsplit('.', 1)[0]  # 提取'3-2'这样的页码  
+        next_page_number = self.get_next_page_number(page_match)  
+        # 构造新的URL  
+        base_url = current_url.rsplit(page_match, 1)[0]  # 去掉页码部分  
+        return f"{base_url}{next_page_number}.html"
     def extract_play_url(self, html):
         url_pattern = r'src:\s*"([^"]+)"'
         match = re.search(url_pattern, html)
         return match.group(1) if match else ""
 
-    def parse(self, response):
+    def parse_page(self, response):
+        """
+            分页处理
+        """
         try:
-            
-            data = response.xpath('/html/body/div[1]/div').get()
-            print(data)
-            # vod_title = response.meta['vod_title']
-            # vod_type = response.meta['vod_type']
-            # video_list = response.css('a.search-result-item')
-            # for video in video_list:
-            #     title_text = video.css('.title::text').get()
-            #     type_text = video.css('.search-result-item-header div::text').get()
-            #     if title_text == vod_title and type_text == vod_type:
-            #         user_image_url = response.xpath('/html/body/div[1]/div[3]/div[1]/div[5]/a/img/@src').get()
-            #         parsed_url = urlparse(user_image_url)
-            #         base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
-            #         next_url = ''.join([self.base_url,video.css('.search-result-item::attr(href)').get()])
- 
-            #         yield Request(
-            #         url=next_url,
-            #         callback=self.parse_episodes,
-            #         meta={
-            #             "vod_title": vod_title,
-            #             "vod_pic_url": ''.join([base_url,video.css('img::attr(data-original)').get()]),
-            #             "vod_type":vod_type,
-            #             "vod_tag":'/'.join(video.css('.tags span::text').getall()),
-            #             "vod_content":video.css('.desc::text').get()
-            #         }
-            #     )
+            video_list = response.css('div.stui-vodlist__box')
+            video_page_total = len(video_list)
+
+            for video in video_list:
+                detail_page_url = ''.join([self.base_url,video.css('a.stui-vodlist__thumb::attr(href)').get()])
+                
+                yield Request(
+                    url=detail_page_url,
+                    callback=self.parse_video,
+                    meta= {
+                        "vod_title": video.css('h4.title a::attr(title)').get(),
+                        "vod_pic_url": video.css('a.stui-vodlist__thumb::attr(data-original)').get(),
+                        "vod_total_episodes": video.css('a.stui-vodlist__thumb span::text').get()
+                    }
+                )
+
+            if video_page_total == 12:
+                yield Request(
+                    url=self.get_next_page_url(response.url),
+                    callback=self.parse_page,
+                    meta=response.meta
+                )
         except Exception as e:
             print(f"Error while parsing response for URL: {response.url}. Error: {e}")
             # 可以在这里添加重试机制，例如使用 response.request.meta['retry_times'] 来控制重试次数
+
+
+    def parse_video(self, response):
+        """
+        爬取视频详情
+        """
+        vod_tag = response.css('div.stui-content__detail p.data::text').get()
+        vod_content = response.css('span.detail-content::text').get()
+        play_url = ''.join([self.base_url,response.css('div.play-btn a::attr(href)').get()])
+        yield Request(
+                    url=play_url,
+                    callback=self.parse_player,
+                    meta={
+                        "vod_tag":vod_tag,
+                        "vod_content":vod_content,
+                        "play_page":play_url
+                    }
+                )
 
     def parse_episodes(self,response):
         """
